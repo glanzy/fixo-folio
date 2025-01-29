8b02dcf087f23da7c082331015d6f36de1177568
thepallavjha1 <emaitopallavjha@gmail.com>
2025-01-29 11:41:10 +0530

    LIVE DB CONNECTED

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://rfxabyatlekcmoagehdd.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJmeGFieWF0bGVrY21vYWdlaGRkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgwNTI1MDIsImV4cCI6MjA1MzYyODUwMn0.mtyTT_JBSBB2qb3Gswzhr8pMEJi19P77IyZWge1qpC4';
+const SUPABASE_URL = 'https://ofxjlgqwxpflchfdimom.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9meGpsZ3F3eHBmbGNoZmRpbW9tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgxMjk4MjcsImV4cCI6MjA1MzcwNTgyN30.nT_7dBhywGBB1uO5evg3ByuBeIR_0k3EWyl9kocIhIY';
 
 if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
   throw new Error('Missing Supabase environment variables');
